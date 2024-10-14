fdc3d4eb1745f9aafd753e72b4ba3a63d5a29568
Ian Eastin <foldsoffury@gmail.com>
2024-10-14 15:40:10 -0500

    Fix null check

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup() {
         cells[i] = [dim];
         for (let j = 0; j < dim; j++) {
 
-            if (saveFile == !null) {
+            if (saveFile != null) {
                 cells[i][j] = new Cell(saveFile[i][j].x, saveFile[i][j].y, saveFile[i][j].w);
                 cells[i][j].col = saveFile[i][j].col;
                 cells[i][j].isFormatBit = saveFile[i][j].isFormatBit;
